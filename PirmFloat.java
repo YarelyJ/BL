2e1f069465005d7d131783114369e8a1ed149155
YarelyJ <154484018+YarelyJ@users.noreply.github.com>
2023-12-29 19:40:57 -0600

    Add files via upload

---
+++ PirmFloat.java
@@ -0,0 +1,6 @@
+public class PirmFloat {
+    public static void main(String[] args) {
+        float realFloat = 0.00000000015f; //1.5e-10f;
+        System.out.println("realFloat = "+ realFloat);
+    }
+}
