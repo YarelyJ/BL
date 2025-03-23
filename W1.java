10e3db27bb117f80e230adb7ca3d362efcce4885
YarelyJ <154484018+YarelyJ@users.noreply.github.com>
2025-03-22 22:02:43 -0600

    Add files via upload

---
+++ W1.java
@@ -0,0 +1,13 @@
+package com.tesji.formulariotda;
+import javax.swing.JOptionPane;
+public class W1 {
+    public static void main(String[] args) {
+        short x;
+        x=1;
+        while (x<=500){
+            System.out.print(x);
+            System.out.print("\n");
+            x= (short) (x+1);
+        }
+    }
+}
