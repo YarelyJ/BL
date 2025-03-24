6d13a57f1f6357d7fef53fdb89f0bb9fba932601
YarelyJ <154484018+YarelyJ@users.noreply.github.com>
2025-03-23 19:18:11 -0600

    Add files via upload

---
+++ w2.java
@@ -0,0 +1,13 @@
+package com.tesji.formulariotda;
+import javax.swing.JOptionPane;
+public class w2 {
+    public static void main(String[] args) {
+        short x;
+        x=50;
+        while (x<=100){
+            System.out.print(x);
+            System.out.print("\n");
+            x= (short) (x+1);
+        }
+    }
+}
