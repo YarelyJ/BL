ba9224b7fc98bf3eaf663fc25af96ced375f8229
YarelyJ <154484018+YarelyJ@users.noreply.github.com>
2025-03-25 01:32:19 +0000

    Add files via upload

---
+++ W3.java
@@ -0,0 +1,13 @@
+package com.tesji.formulariotda;
+
+public class W3 {
+    public static void main(String[] args) {
+        short x;
+        x=-50;
+        while (x<=0){
+            System.out.print(x);
+            System.out.print("\n");
+            x= (short) (x+1);
+        }
+    }
+}
